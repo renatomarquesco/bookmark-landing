555d207739975af84d5e382daf7511ddda696faa
Renato Marques <67842526+renatomarquesco@users.noreply.github.com>
2021-08-20 19:07:41 +0200

    Update script.js

--- bookmark-landing-page-master/script.js
+++ bookmark-landing-page-master/script.js
@@ -8,7 +8,9 @@ let textFeat = document.getElementById("text-feat")
 let titleFeat = document.getElementById("title-feat");
 let pics = ["./images/illustration-features-tab-1.svg", "./images/illustration-features-tab-2.svg", "./images/illustration-features-tab-3.svg"]
 let h2s = ["Bookmark in one click", "Intelligent search", "Share your bookmarks"]
-let text = ["Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.", "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.", "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."]
+let text = ["Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites.", 
+            "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks.", 
+            "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."]
 
 hamb.forEach((btn) => {
     btn.addEventListener("click", function () {
